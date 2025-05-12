cc6b36277c01f7655b17d8ed7b0cd17a0dab9a33
maxhome <arshadvk7560@gmail.com>
2025-05-12 18:03:04 +0400

    committ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,17 @@ import Header from "@/layout/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
+const noLayoutRoutes = ['/login', '/signup']; // add paths here
+
+export default function App({ Component, pageProps ,router }: AppProps) {
+  const isNoLayout = noLayoutRoutes.includes(router.pathname);
+  
+    return isNoLayout ? (
+      <Component {...pageProps} />
+    ) : ( <>
     <Header />
     <Component {...pageProps} />
     <Footer />
-    </>
-)
+    </>);
+
 }
